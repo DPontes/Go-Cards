9cc04543f54db188d9873325485a9d8068102c4b
DPontes <dpontes@protonmail.com>
2021-04-24 21:39:43 +0200

    Implementation of a 'receiver' function

--- deck.go
+++ deck.go
@@ -1,7 +1,13 @@
 package main
 
+import "fmt"
+
 // create a new type of `deck`
 // which is a slice of strings
 type deck []string
 
-
+func (d deck) print() {     // this is a "receiver" function
+  for i, card := range d {
+    fmt.Println(i, card)
+  }
+}
