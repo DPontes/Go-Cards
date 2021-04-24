1f8d52c4546d6001948e65283b57132abe2ab04c
DPontes <dpontes@protonmail.com>
2021-04-24 22:05:14 +0200

    added a comment to the receiver function

--- deck.go
+++ deck.go
@@ -6,7 +6,15 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
-func (d deck) print() {     // this is a "receiver" function
+/*
+  Receiver function
+  The variable 'd' is the same as
+  having the variable 'cards', but 
+  by convention it is used to first
+  one or two letters of the variable
+  type; in this case, 'd' for type 'deck'
+*/
+func (d deck) print() {
   for i, card := range d {
     fmt.Println(i, card)
   }
