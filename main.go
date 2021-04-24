e85693147bb484bb9f570e6bd2c82a105c717491
DPontes <dpontes@protonmail.com>
2021-04-24 13:17:02 +0200

    Added a function newCard

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import "fmt"
 
 func main() {
   /*
-    The following two lines do the same,
+    The following two lines are equivalent,
     but the first one will throw a "sugestion"
     in modern IDEs as it considers redundant to
     assign `string` to the variable as it's
     inferred by the RHL
   */
-  // var card string= "Ace of Spades"
-  card := "Ace of Spades"
+  // var card string= newCard()
+  card := newCard()   // := operator can only be used during new variable assignment
+
   fmt.Println(card)
+}
+
+func newCard() string {
+  return "Five of Diamonds"
 }
