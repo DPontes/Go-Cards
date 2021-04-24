6150f9177e3a19bf092aa5f435e64b909a233519
DPontes <dpontes@protonmail.com>
2021-04-24 13:01:40 +0200

    Function prints a string

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Cards Game in Golang
+# Cards Game in Golang
 
 This code is a project from the Udemy course "Go: The Complete Developer's Guide"
--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+  /*
+    The following two lines do the same,
+    but the first one will throw a "sugestion"
+    in modern IDEs as it considers redundant to
+    assign `string` to the variable as it's
+    inferred by the RHL
+  */
+  // var card string= "Ace of Spades"
+  card := "Ace of Spades"
+  fmt.Println(card)
+}
